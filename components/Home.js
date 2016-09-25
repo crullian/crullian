d9ef780755daf51a919ef9a00d51ae2206d22526
crullian <crullian@gmail.com>
2016-09-24 23:03:27 -0400

    Change span to div, improve className.

--- components/Home.js
+++ components/Home.js
@@ -7,9 +7,9 @@ export default class Home extends React.Component {
     let content = this.props.children;
     if(this.props.location.pathname == '/') {
       content = (
-          <div className="theMan">
-            <span>Hello World</span>
-            <span><img src="assets/z.jpg" className="image"/></span>
+          <div className="man">
+            <div>Hi! Thanks for visiting. I'm a Graduate, Fullstack Academy of Code Software Engineering Immersive Program. Experience building client and server side Web applications using the following technologies</div>
+            <div><img src="assets/z.jpg" className="image"/></div>
           </div>
       )
     }
